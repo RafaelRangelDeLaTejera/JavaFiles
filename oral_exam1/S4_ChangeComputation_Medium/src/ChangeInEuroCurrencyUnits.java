10c7679bf04d5eb233cdd39504ee7f83d81e685e
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-09-06 18:09:21 -0500

    project 1 commit two with price functionality

---
+++ oral_exam1/S4_ChangeComputation_Medium/src/ChangeInEuroCurrencyUnits.java
@@ -0,0 +1,190 @@
+public class ChangeInEuroCurrencyUnits {
+
+    private int totalEuroCents = 0;
+    private int wholeEuros = 0;
+    private int centEuros = 0;
+
+    public ChangeInEuroCurrencyUnits(CurrencyUSD changeInDollars, int usedTwentyDollarBills, int usedTenDollarBills, int usedFiveDollarBills, int usedOneDollarBills, int usedQuarters, int usedDimes, int usedNickels, int usedPennies) {
+
+        changeUSDToEuros(changeInDollars);
+        printEuroUnits(usedTwentyDollarBills,usedTenDollarBills,usedFiveDollarBills,usedOneDollarBills,usedQuarters,usedDimes,usedNickels,usedPennies);
+    }
+
+    public int getTotalEuroCents() {
+        return totalEuroCents;
+    }
+
+    public void setTotalEuroCents(int totalEuroCents) {
+        this.totalEuroCents = totalEuroCents;
+    }
+
+    public int getWholeEuros() {
+        return wholeEuros;
+    }
+
+    public void setWholeEuros(int wholeEuros) {
+        this.wholeEuros = wholeEuros;
+    }
+
+    public int getCentEuros() {
+        return centEuros;
+    }
+
+    public void setCentEuros(int centEuros) {
+        this.centEuros = centEuros;
+    }
+
+    public void changeUSDToEuros(CurrencyUSD dollarsToChange){
+        /** convert dollars to Euros with conversion factor of 1 USD = 0.8419 Euros and keeping the int precision of using whole units and cents of currency separately to avoid double or float imprecision */
+        int totalAmountOfCentEuros = (dollarsToChange.getPriceDollars() * 8419) / 100 + (dollarsToChange.getPriceCents() * 8419) / 10000;
+
+        this.setTotalEuroCents(totalAmountOfCentEuros);
+
+        int resultWholeEuros = totalAmountOfCentEuros / 100; //dont need to worry about USD cent because they cant add up to one euro since one dollar is just 0.84 of a dollar
+        int resultCentEuros = totalAmountOfCentEuros % 100;
+
+        /**check if exact amount was reached, if not then go over by smallest amount (one cent Euro)*/
+        if (((dollarsToChange.getPriceDollars() * 8419) % 100) != 0 || ((dollarsToChange.getPriceCents() * 8419) % 10000 != 0)) ;
+        {
+            resultCentEuros++;
+
+            if (resultCentEuros % 100 == 0) //in case the cents added up to one Euro when the cent was added
+            {
+                resultCentEuros = 0;
+                resultWholeEuros++;
+            }
+        this.setWholeEuros(resultWholeEuros);
+        this.setCentEuros(resultCentEuros);
+
+        }
+    }
+
+    public void printEuroUnits(int twentyBills, int tenBills, int fiveBills, int oneBills, int twentyFiveCent, int tenCent, int fiveCent, int oneCent){
+        /**determine which bills and coins will be used to pay back */
+        if (twentyBills !=0 && this.getTotalEuroCents()/2000 > 0){
+            if (twentyBills >= this.getTotalEuroCents()/2000){
+                int noOfTwentyEuroBills = this.getTotalEuroCents()/2000;
+                setTotalEuroCents(getTotalEuroCents()%2000);
+            }
+            else if (twentyBills < this.getTotalEuroCents()/2000){
+                int noOfTwentyEuroBills = twentyBills;
+                setTotalEuroCents(getTotalEuroCents()-2000*noOfTwentyEuroBills);
+            }
+        }
+        else if (tenBills != 0 && this.getTotalEuroCents()/1000 > 0){
+            int noOfTenEuroBills = this.getTotalEuroCents()/1000;
+            setTotalEuroCents(getTotalEuroCents()%1000);
+        }
+        else if (fiveBills != 0 && this.getTotalEuroCents()/500 > 0){
+            int noOfFiveEuroBills = this.getTotalEuroCents()/500;
+            setTotalEuroCents(getTotalEuroCents()%500);
+        }
+        else if (oneBills != 0 && this.getTotalEuroCents()/100 > 0){
+            int noOfOneDollarBills = this.getTotalEuroCents()/100;
+            setTotalEuroCents(getTotalEuroCents()%100);
+        }
+        else if (tenCent !=0 && this.getTotalEuroCents()/10 > 0){
+            int noOfTenCentCoins = this.getTotalEuroCents()/10;
+            setTotalEuroCents(getTotalEuroCents()%10);
+        }
+        else if (fiveBills != 0 && this.getTotalEuroCents()/5 > 0){
+            int noOfOneDollarBills = this.getTotalEuroCents()/5;
+            setTotalEuroCents(getTotalEuroCents()%5);
+        }
+        else if (oneCent != 0 && this.getTotalEuroCents() > 0){
+            int noOfOneCentCoins = this.getTotalEuroCents();
+        }
+    };
+}
+
+
+
+
+
+/*//amount of euro bills to be returned
+    private int twentyEuroBills;
+    private int tenEuroBills;
+    private int fiveEuroBills;
+    private int oneEuroBill;
+    private int fiftyCentEuroCoin;
+    private int twentyCentEuroCoin;
+    private int tenCentEuroCoin;
+    private int fiveCentEuroCoin;
+    private int oneCentEuroCoin;
+
+    *//** setter and getter for twentyEuroBills *//*
+    public int getTwentyEuroBills() {
+        return twentyEuroBills;
+    }
+
+    public void setTwentyEuroBills(int twentyEuroBills) {
+        this.twentyEuroBills = twentyEuroBills;
+    }
+    *//** setter and getter for tenEuroBills *//*
+    public int getTenEuroBills() {
+        return tenEuroBills;
+    }
+
+    public void setTenEuroBills(int tenEuroBills) {
+        this.tenEuroBills = tenEuroBills;
+    }
+    *//** setter and getter for fiveEuroBills *//*
+    public int getFiveEuroBills() {
+        return fiveEuroBills;
+    }
+
+    public void setFiveEuroBills(int fiveEuroBills) {
+        this.fiveEuroBills = fiveEuroBills;
+    }
+    *//** setter and getter for oneEuroBill *//*
+    public int getOneEuroBill() {
+        return oneEuroBill;
+    }
+
+    public void setOneEuroBill(int oneEuroBill) {
+        this.oneEuroBill = oneEuroBill;
+    }
+
+    *//** setter and getter for fiftyCentEuroCoin *//*
+    public int getFiftyCentEuroCoin() {
+        return fiftyCentEuroCoin;
+    }
+
+    public void setFiftyCentEuroCoin(int fiftyCentEuroCoin) {
+        this.fiftyCentEuroCoin = fiftyCentEuroCoin;
+    }
+    *//** setter and getter for twentyCentEuroCoin *//*
+    public int getTwentyCentEuroCoin() {
+        return twentyCentEuroCoin;
+    }
+
+    public void setTwentyCentEuroCoin(int twentyCentEuroCoin) {
+        this.twentyCentEuroCoin = twentyCentEuroCoin;
+    }
+    *//** setter and getter for tenCentEuroCoin *//*
+    public int getTenCentEuroCoin() {
+        return tenCentEuroCoin;
+    }
+
+    public void setTenCentEuroCoin(int tenCentEuroCoin) {
+        this.tenCentEuroCoin = tenCentEuroCoin;
+    }
+
+    *//** setter and getter for fiveCentEuroCoin *//*
+    public int getFiveCentEuroCoin() {
+        return fiveCentEuroCoin;
+    }
+
+    public void setFiveCentEuroCoin(int fiveCentEuroCoin) {
+        this.fiveCentEuroCoin = fiveCentEuroCoin;
+    }
+
+    *//** setter and getter for oneCentEuroCoin*//*
+    public int getOneCentEuroCoin() {
+        return oneCentEuroCoin;
+    }
+
+    public void setOneCentEuroCoin(int oneCentEuroCoin) {
+        this.oneCentEuroCoin = oneCentEuroCoin;
+    }
+*/
