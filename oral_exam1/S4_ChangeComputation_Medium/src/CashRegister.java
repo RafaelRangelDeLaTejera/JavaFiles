a511d6266a0c5afb0942b8d9b3327ba379a0d4ab
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-09-05 19:22:00 -0500

    project 1 commit two with price functionality

--- oral_exam1/S4_ChangeComputation_Medium/src/CashRegister.java
+++ oral_exam1/S4_ChangeComputation_Medium/src/CashRegister.java
@@ -2,10 +2,6 @@ import java.util.Locale;
 
 public class CashRegister {
 
-    /**class private member variables */
-    private int dollarsPaid;
-    private int centsPaid;
-
     /**
      *
      */
@@ -16,31 +12,40 @@ public class CashRegister {
         /** get whole units and convert the string value to an int value */
         String wholeNumber = price.substring(0, price.indexOf("."));
         System.out.println(wholeNumber);
-        int wholeUSDAmount = Integer.parseInt(wholeNumber);
+        int priceDollars = Integer.parseInt(wholeNumber);
 
-        //** get decimal units from input string and convert them to integers, this will be the cents
+        /** get decimal units from input string and convert them to integers, this will be the cents */
         String decimalNumber = price.substring(price.indexOf(".")+1);
         System.out.println(decimalNumber);
-        int decimalUSDAmount = Integer.parseInt(decimalNumber);
+        int priceCents = Integer.parseInt(decimalNumber);
 
-        System.out.println(wholeUSDAmount + "." + decimalUSDAmount);
+        /**get the sum of the dollars and cents the costumer paid using the sumDollars and sumCents instance methods*/
+        /**class private member variables */
+        int dollarsPaid = sumDollars(twentyDollarBills, tenDollarBills, fiveDollarBills, oneDollarBills, quarters, dimes, nickels, pennies);
+        int centsPaid = sumCents(quarters, dimes, nickels, pennies);
 
+       /**Get the amount of change in USD that that will be returned to the user*/
+        int changeInUSDollars = dollarsPaid - priceDollars;
+        int changeInUSCents = centsPaid - priceCents;
 
     }
 
     public int sumDollars(int twentyDollars, int tenDollars, int fiveDollars, int oneDollar, int twentyFiveCents, int tenCents, int fiveCents, int oneCent) {
-        int sum = 0;
-
-        /**get the sum of the dollars from the cents, if they ad up to dollars*/
 
 
-        sum = twentyDollars + tenDollars + fiveDollars + oneDollar;
+        /**get the sum of the dollars from the cents, by summing all the cents up and dividing them by a 100 using int division, since a 100 cents are 1 dollar*/
+        int dollarCentSum = (twentyFiveCents + tenCents + fiveCents + oneCent)/100;
+
+        int sum = twentyDollars + tenDollars + fiveDollars + oneDollar + dollarCentSum;
 
         return sum;
     }
 
-    public void sumCents(int twentyFiveCents, int tenCents, int fiveCents, int oneCent) {
-        //devide and use %
+    public int sumCents(int twentyFiveCents, int tenCents, int fiveCents, int oneCent) {
+        /** get the total remaining cents on the amount paid with using the modulus operand */
+        int centSum = (twentyFiveCents + tenCents + fiveCents + oneCent)%100;
+
+        return centSum;
     }
 }
 
