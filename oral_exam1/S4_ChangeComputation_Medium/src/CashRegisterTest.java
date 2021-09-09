c81c139eb2aafcc647ace99f34be056e2194c10d
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-09-09 12:13:44 -0500

    project 1 demo first tests past now use unit testing

--- oral_exam1/S4_ChangeComputation_Medium/src/CashRegisterTest.java
+++ oral_exam1/S4_ChangeComputation_Medium/src/CashRegisterTest.java
@@ -9,6 +9,6 @@ class CashRegisterTest {
     @Test
     void testOneCashRegister() {
         assertEquals("Please return to costumer, 1 Fifty Euro Cents", cashRegister.computeChange("14.41",0,1,1,0,0,0,0,0));
-        assertEquals("Please return to costumer, 2 twenty Euro Bills, 571 one Euro Cents",cashRegister.computeChange("46.85",4,1,0,0,0,0,0,20));
+        assertEquals("Please return to costumer, 1 twenty Euro Bills, 1 Ten Euro Bills, 1 Five Euro Bills, 1 One Euro Bills, 1 twenty Euro Cents, 1 ten Euro Cents, 20 One Euro Cents",cashRegister.computeChange("46.85",4,1,0,0,0,0,0,20));
     }
 }
