2f4cc59d75ebc8b71e5f7fbaeb827a4f2584ecc6
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-11-04 15:12:26 -0500

    second project

--- quizzes/QuizCalculator/src/CalculatorController.java
+++ quizzes/QuizCalculator/src/CalculatorController.java
@@ -1,11 +1,17 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CalculatorController {
 
-    String text = "";
+    String operations = "";
+    double value = 0;
 
     @FXML
     private Button sevenButton;
@@ -72,103 +78,161 @@ public class CalculatorController {
 
     @FXML
     void ceroPressed(ActionEvent event) {
-
+        if (operations.equals("")){
+            operations= "";
+        }
+        else {
+            operations += "0";
+        }
+        result.setText(operations);
 
     }
 
     @FXML
     private void clearPressed(ActionEvent event) {
+        operations = "";
+        value = 0;
+        result.setText(operations);
 
     }
 
     @FXML
     void cosPressed(ActionEvent event) {
+        value = CalculatorModel.computeResult(operations);
+        value = Math.cos(value);
+        result.setText(String.valueOf(value));
 
     }
 
     @FXML
     void dividePressed(ActionEvent event) {
-
+        if (operations.equals("")){
+            operations= "";
+        }
+        else {
+            operations += "/";
+        }
+        result.setText(operations);
     }
 
     @FXML
     void eightPressed(ActionEvent event) {
-
+        operations +="8";
+        result.setText(operations);
     }
 
     @FXML
     void equalPressed(ActionEvent event) {
-
+        value = CalculatorModel.computeResult(operations);
+        result.setText(String.valueOf(value));
+        operations = "";
     }
 
     @FXML
     void exponentPresed(ActionEvent event) {
-
+        value = CalculatorModel.computeResult(operations);
+        value = Math.exp(value);
+        result.setText(String.valueOf(value));
+        operations = "";
     }
 
     @FXML
     void fivePressed(ActionEvent event) {
-
+        operations += "5";
+        result.setText(operations);
     }
 
     @FXML
     void fourPressed(ActionEvent event) {
-
+        operations += "4";
+        result.setText(operations);
     }
 
     @FXML
     void minusPressed(ActionEvent event) {
-
+        if (operations.equals("")){
+            operations= "";
+        }
+        else {
+            operations += "-";
+        }
+        result.setText(operations);
     }
 
     @FXML
     void multiplicationPressed(ActionEvent event) {
-
+        if (operations.equals("")){
+            operations= "";
+        }
+        else {
+            operations += "*";
+        }
+        result.setText(operations);
     }
 
     @FXML
     void ninePressed(ActionEvent event) {
-
+        operations += "9";
+        result.setText(operations);
     }
 
     @FXML
     void onePressed(ActionEvent event) {
-
+        operations += "1";
+        result.setText(operations);
     }
 
     @FXML
     void plusPressed(ActionEvent event) {
-
+        if (operations.equals("")){
+            operations= "";
+        }
+        else {
+            operations += "+";
+        }
+        result.setText(operations);
     }
 
     @FXML
     void sevenPressed(ActionEvent event) {
-
+        operations += "7";
+        result.setText(operations);
     }
 
     @FXML
     void sinButton(ActionEvent event) {
-
+        value = CalculatorModel.computeResult(operations);
+        value = Math.sin(value);
+        result.setText(String.valueOf(value));
+        operations = "";
     }
 
     @FXML
     void sixPressed(ActionEvent event) {
-
+        operations += "6";
+        result.setText(operations);
     }
 
     @FXML
     void squarePressed(ActionEvent event) {
-
+        value = CalculatorModel.computeResult(operations);
+        value = value*value;
+        result.setText(String.valueOf(value));
+        operations = "";
     }
 
     @FXML
     void threePressed(ActionEvent event) {
-
+        operations += "3";
+        result.setText(operations);
     }
 
     @FXML
     void twoPressed(ActionEvent event) {
-
+        operations += "2";
+        result.setText(operations);
     }
+
+
 
 }
--- quizzes/QuizCalculator/src/CalculatorModel.java
+++ quizzes/QuizCalculator/src/CalculatorModel.java
@@ -1,2 +1,79 @@
 public class CalculatorModel {
+//    private double resultValue = 0;
+//    private String operationsAndValues = "";
+
+//    public CalculatorModel() {
+//        resultValue = 0;
+//        operationsAndValues = "";
+//    }
+
+//    public static double getResultValue() {
+//        return resultValue;
+//    }
+//
+//    public void setResultValue(double resultValue) {
+//        this.resultValue = resultValue;
+//    }
+
+//    public String getOperationsAndValues() {
+//        return operationsAndValues;
+//    }
+//
+//    public void setOperationsAndValues(String operationsAndValues) {
+//        this.operationsAndValues += operationsAndValues;
+//    }
+
+    public static double computeResult(String operationsAndValues){
+        String values = "";
+        double resultValue = 0;
+
+        for (int i = 0; i < operationsAndValues.length(); i++){
+            if (Character.isDigit(operationsAndValues.charAt(i))){
+                values+= operationsAndValues.charAt(i);
+                resultValue = Double.parseDouble(values);
+            }
+            else if (operationsAndValues.charAt(i) == '+'){
+                i++;
+                values = "";
+                while (Character.isDigit(operationsAndValues.charAt(i))){
+                    values+= operationsAndValues.charAt(i);
+                    i++;
+                }
+                double sum = Double.parseDouble(values);
+                resultValue += sum;
+            }
+            else if (operationsAndValues.charAt(i) == '-'){
+                i++;
+                values = "";
+                while (Character.isDigit(operationsAndValues.charAt(i))){
+                    values+= operationsAndValues.charAt(i);
+                    i++;
+                }
+                double substraction = Double.parseDouble(values);
+                resultValue -= substraction;
+            }
+            else if (operationsAndValues.charAt(i) == '*'){
+                i++;
+                values = "";
+                while (Character.isDigit(operationsAndValues.charAt(i))){
+                    values+= operationsAndValues.charAt(i);
+                    i++;
+                }
+                double times = Double.parseDouble(values);
+                resultValue *= times;
+            }
+            else if (operationsAndValues.charAt(i) == '/'){
+                i++;
+                values = "";
+                while (Character.isDigit(operationsAndValues.charAt(i))){
+                    values+= operationsAndValues.charAt(i);
+                    i++;
+                }
+                double divide = Double.parseDouble(values);
+                resultValue /= divide;
+            }
+        }
+        return resultValue;
+    }
+
 }
