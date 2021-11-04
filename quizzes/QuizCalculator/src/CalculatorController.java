ae5fa7fa978eb6a47b506558ca9082efe16fe92b
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-11-04 12:01:04 -0500

    second project

--- quizzes/QuizCalculator/src/CalculatorController.java
+++ quizzes/QuizCalculator/src/CalculatorController.java
@@ -1,2 +1,69 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CalculatorController {
+
+    @FXML
+    private TextField sevenButton;
+
+    @FXML
+    private TextField eightButton;
+
+    @FXML
+    private TextField nineButton;
+
+    @FXML
+    private TextField divideButton;
+
+    @FXML
+    private TextField fourButton;
+
+    @FXML
+    private TextField fiveButton;
+
+    @FXML
+    private TextField sixButton;
+
+    @FXML
+    private TextField multiplyButton;
+
+    @FXML
+    private TextField oneButton;
+
+    @FXML
+    private TextField ceroButton;
+
+    @FXML
+    private TextField twoButton;
+
+    @FXML
+    private TextField clearButton;
+
+    @FXML
+    private TextField threeButton;
+
+    @FXML
+    private TextField equalButton;
+
+    @FXML
+    private TextField plusButton;
+
+    @FXML
+    private TextField minusButton;
+
+    @FXML
+    private TextField expButton;
+
+    @FXML
+    private TextField cosButton;
+
+    @FXML
+    private TextField sinButton;
+
+    @FXML
+    private TextField squareButton;
+
+    @FXML
+    private TextField result;
+
 }
