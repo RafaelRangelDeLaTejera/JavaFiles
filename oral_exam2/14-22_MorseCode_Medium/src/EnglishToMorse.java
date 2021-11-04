7f8377af2759422b1325be6d1dafbc379fae1bdc
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2021-11-03 20:32:34 -0500

    extra credit

---
+++ oral_exam2/14-22_MorseCode_Medium/src/EnglishToMorse.java
@@ -0,0 +1,20 @@
+public class EnglishToMorse {
+
+    private String encodedPhrase;
+    private static final String[] code = {".-","=...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",
+            ".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----",
+            "..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+
+
+    public EnglishToMorse() {
+        encodedPhrase = "";
+    }
+
+    public String encodeToMorse(String englishPhrase){
+
+        for(int i = 0; i < englishPhrase.length(); i++){
+
+        }
+        return encodedPhrase;
+    }
+}
