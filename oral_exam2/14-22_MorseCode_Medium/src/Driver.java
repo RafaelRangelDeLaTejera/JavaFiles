242c087b00a452309f0d3d997dbf934c7c7b9898
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2021-11-03 20:54:54 -0500

    extra credit

--- oral_exam2/14-22_MorseCode_Medium/src/Driver.java
+++ oral_exam2/14-22_MorseCode_Medium/src/Driver.java
@@ -1,6 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
 
+        EnglishToMorse testEnToMorse = new EnglishToMorse();
+
+        System.out.print(testEnToMorse.encodeToMorse("Hello 12 p"));
 
     }
 }
--- oral_exam2/14-22_MorseCode_Medium/src/EnglishToMorse.java
+++ oral_exam2/14-22_MorseCode_Medium/src/EnglishToMorse.java
@@ -1,9 +1,9 @@
 public class EnglishToMorse {
 
     private String encodedPhrase;
-    private static final String[] code = {".-","=...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",
-            ".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----",
-            "..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+    private static final String[] code = {"-----",".----",
+            "..---","...--","....-",".....","-....","--...","---..","----.",".-","=...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",
+            ".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
 
     public EnglishToMorse() {
@@ -13,7 +13,24 @@ public class EnglishToMorse {
     public String encodeToMorse(String englishPhrase){
 
         for(int i = 0; i < englishPhrase.length(); i++){
-
+            if (englishPhrase.charAt(i) == ' '){
+                encodedPhrase += "  ";
+            }
+            else {
+                int characterIndex = englishPhrase.charAt(i);
+                if (characterIndex > 65 && characterIndex < 90){
+                    encodedPhrase = encodedPhrase + code[characterIndex-55] + " ";
+                }
+                else if (characterIndex < 10){
+                    encodedPhrase = encodedPhrase + code[characterIndex] + " ";
+                }
+                else if (characterIndex > 97 && characterIndex < 122){
+                    encodedPhrase = encodedPhrase + code[characterIndex-87] + " ";
+                }
+                else {
+                    encodedPhrase = encodedPhrase + englishPhrase.substring(i,i);
+                }
+            }
         }
         return encodedPhrase;
     }
