a0594ba5b60bce9be48b853d4c16281afded6827
RafaelRangelDeLaTejera <rlrangeldelatejera@gmail.com>
2021-11-19 11:21:11 -0600

    project 4 done

--- oral_exam2/S28_MultithreadingRootFinder_Medium/src/Master.java
+++ oral_exam2/S28_MultithreadingRootFinder_Medium/src/Master.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -7,45 +8,76 @@ public class Master {
 
     public static void main(String[] args) throws InterruptedException{
 
+        Random randomNumber = new Random();//object to create random numbers for coefficients
+
+        //have user select option
+        Scanner input = new Scanner(System.in); //create scanner object to read in from user
+        System.out.print("Input 1 to generate and solve 30 sets of randomly generated coefficients and print all roots," +
+                "or input 2 to generate and solve 3000 sets of coefficients and print Statistics of slave thread usage");
+
+        int option = input.nextInt();
+
         ExecutorService executorService = Executors.newCachedThreadPool();
 
 
          CircularBufferCoefficients sharedBufferCoefficients = new CircularBufferCoefficients();
          CircularBufferRoots sharedBufferRoots = new CircularBufferRoots();
 
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
-        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 1));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 2));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 3));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 4));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 5));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 6));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 7));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 8));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 9));
+        executorService.execute(new Slave(sharedBufferCoefficients,sharedBufferRoots, 10));
 
         executorService.awaitTermination(1, TimeUnit.MILLISECONDS); //wait for the 10 slaves to  be created
 
+        if (option == 1) {
+            for (int i = 0; i < 30; i++) {
 
-        Random randomNumber = new Random();//object to create random numbers for coefficients
+                try {
+                    System.out.print(i);
 
-        for (int i = 0; i < 30; i++) {
+                    Thread.sleep(100);
+                    sharedBufferCoefficients.putCoefficients(1, 4, 4);
 
-            try {
-                System.out.print(i);
-
-                Thread.sleep(100);
-                sharedBufferCoefficients.putCoefficients(1, 4, 4);
-
-                Thread.sleep(100);
-                String[] roots = sharedBufferRoots.getRoots();
-                System.out.print("root 1 = " + roots[0] + ", root 2 = " + roots[1] + "\n");
+                    Thread.sleep(100);
+                    String[] roots = sharedBufferRoots.getRoots();
+                    System.out.print("root 1 = " + roots[0] + ", root 2 = " + roots[1] + "\n");
 
 
-            } catch (Exception e) {
-                System.out.print("In exception master");
-                // Thread.currentThread().interrupt();
+                } catch (Exception e) {
+                    System.out.print("In exception master");
+                    // Thread.currentThread().interrupt();
+                }
             }
+        }
+        else if (option == 2){
+            for (int i = 0; i < 3000; i++) {
+
+                try {
+                    System.out.print(i);
+
+                    sharedBufferCoefficients.putCoefficients(1, 4, 4);
+
+                    String[] roots = sharedBufferRoots.getRoots();
+                   // System.out.print("root 1 = " + roots[0] + ", root 2 = " + roots[1] + "\n");
+
+
+                } catch (Exception e) {
+                    System.out.print("In exception master");
+                    // Thread.currentThread().interrupt();
+                }
+            }
+
+
+        }
+        else {
+            System.out.print("invalid input, run program again");
         }
 
         executorService.awaitTermination(1, TimeUnit.MILLISECONDS);
--- oral_exam2/S28_MultithreadingRootFinder_Medium/src/Slave.java
+++ oral_exam2/S28_MultithreadingRootFinder_Medium/src/Slave.java
@@ -1,76 +1,78 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
-
 public class Slave implements Runnable{
 
     private final CircularBufferCoefficients sharedBufferCoefficients; // reference to shared object
     private final CircularBufferRoots sharedBufferRoots;
-    //private static boolean control;
+    private final int slaveNo;
+    private int countSolvedSets;
 
-    public Slave(CircularBufferCoefficients circularBufferCoefficients,CircularBufferRoots sharedBufferRoots) {
+    public Slave(CircularBufferCoefficients circularBufferCoefficients, CircularBufferRoots sharedBufferRoots, int slaveNo) {
         this.sharedBufferCoefficients = circularBufferCoefficients;
         this.sharedBufferRoots = sharedBufferRoots;
-        //control = true;
+        this.slaveNo = slaveNo;
+        countSolvedSets = 0;
     }
 
     @Override
     public void run() {
 
         while(true){
-        try {
+            try {
 
-            //Thread.sleep(100);
+                Thread.sleep(100);
 
-            double[] coefficients = sharedBufferCoefficients.getCoefficients();
+                double[] coefficients = sharedBufferCoefficients.getCoefficients();
 
+                countSolvedSets++;
 
-            double a = coefficients[0]; //get the three coefficient for the quadratic equation
-            double b = coefficients[1];
-            double c = coefficients[2];
-            System.out.print("int slave ");
+                double a = coefficients[0]; //get the three coefficient for the quadratic equation
+                double b = coefficients[1];
+                double c = coefficients[2];
+                System.out.print("int slave ");
 
-            String x1, x2;
+                String x1, x2;
 
-            //perform operations to get the roots
-            double underRootResult = (b * b) - (4 * a * c);
-            double rightPartResult = -b / (2 * a);
+                //perform operations to get the roots
+                double underRootResult = (b * b) - (4 * a * c);
+                double rightPartResult = -b / (2 * a);
 
-            if (underRootResult < 0) { //if result is imaginary number
-                double imaginaryPart = Math.sqrt(-1 * underRootResult) / (2 * a);
+                if (underRootResult < 0) { //if result is imaginary number
+                    double imaginaryPart = Math.sqrt(-1 * underRootResult) / (2 * a);
 
-                x1 = rightPartResult + " + " + imaginaryPart + "i";
-                x2 = rightPartResult + " - " + imaginaryPart + "i";
+                    x1 = rightPartResult + " + " + imaginaryPart + "i";
+                    x2 = rightPartResult + " - " + imaginaryPart + "i";
 
-            } else {
-                double leftPart = Math.sqrt(underRootResult)/(2*a);
-                double root1 = rightPartResult + leftPart;
-                double root2 = rightPartResult - leftPart;
+                } else {
+                    double leftPart = Math.sqrt(underRootResult)/(2*a);
+                    double root1 = rightPartResult + leftPart;
+                    double root2 = rightPartResult - leftPart;
 
-                x1 = Double.toString(root1);
-                x2 = Double.toString(root2);
+                    x1 = Double.toString(root1);
+                    x2 = Double.toString(root2);
+                }
+
+                System.out.print("\nright before put roots\n");
+
+                Thread.sleep(100);
+                sharedBufferRoots.putRoots(x1, x2);
+
+                System.out.print("\nout\n");
+
+            } catch (Exception e) {
+                System.out.print("in exception ");
+                //Thread.currentThread().interrupt();
+
+               // e.printStackTrace();
             }
-
-            System.out.print("\nright before put roots\n");
-
-           // Thread.sleep(100);
-            sharedBufferRoots.putRoots(x1, x2);
-
-            System.out.print("\nout\n");
-
-        } catch (Exception e) {
-            System.out.print("in exception ");
-            //Thread.currentThread().interrupt();
-
-           // e.printStackTrace();
         }
 
+        System.out.print(countSolvedSets + "Sets solved by slave " + slaveNo);
     }
 
-
-
+    public int getCountSolvedSets() {
+        return countSolvedSets;
     }
 
-//    public static void setControl(boolean control) {
-//        Slave.control = control;
-//    }
+    public int getSlaveNo() {
+        return slaveNo;
+    }
 }
