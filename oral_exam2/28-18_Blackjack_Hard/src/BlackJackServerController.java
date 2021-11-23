b59542690e9e1b3b9f434d532edbcaeff9aee95c
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-11-22 19:38:07 -0600

    six project start

---
+++ oral_exam2/28-18_Blackjack_Hard/src/BlackJackServerController.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class BlackJackServerController {
+
+    @FXML
+    private TextArea gameDetails;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+}
+
