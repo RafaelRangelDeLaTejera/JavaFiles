67508ecf0380b06978b147be542e03944ea61a73
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-11-22 19:31:20 -0600

    six project start

---
+++ oral_exam2/28-18_Blackjack_Hard/src/BlackJackClientController.java
@@ -0,0 +1,59 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class BlackJackClientController {
+
+    @FXML
+    private Button hitButton;
+
+    @FXML
+    private Button stayButton;
+
+    @FXML
+    private Button doubleButton;
+
+    @FXML
+    private Button outButton;
+
+    @FXML
+    private TextArea cardDealt;
+
+    @FXML
+    private TextField playerTotal;
+
+    @FXML
+    private TextField dealerTotal;
+
+    @FXML
+    private TextField bet;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void doublePressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hitPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void outPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stayPressed(ActionEvent event) {
+
+    }
+
+}
+
