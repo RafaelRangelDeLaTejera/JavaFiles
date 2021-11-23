7a5ba0c62637c1ac07db9429417c3dff75c8838c
rlrangeldelatejera <rlrangeldelatejera@uiowa.edu>
2021-11-22 20:24:03 -0600

    six project start

---
+++ oral_exam2/28-18_Blackjack_Hard/src/BlackJackClientApp.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BlackJackClientApp extends Application {
+
+    public static void main(String[] args){
+        Application.launch(args); //launch the blackJack player application
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("BlackJackClient.fxml")); //load fxml file
+
+            Scene scene = new Scene(root); //create new scene object with the loaded fxml of the scene builder design
+
+            //set parameters of the stage which is the window that will show our application, and show it
+            stage.setTitle("BlackJack Game");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e){
+            System.out.print(e.getMessage());
+        }
+    }
+}
